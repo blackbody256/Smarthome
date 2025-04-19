b60bfb0052cc5955261e8bfcfc4253d2307e5307
SelinaMasembe <selinamasembe27@gmail.com>
2025-04-19 11:52:01 +0300

    I have added the time picker to the routines page

--- app/src/main/java/com/example/welcome/routine/CreateRoutineScreen.kt
+++ app/src/main/java/com/example/welcome/routine/CreateRoutineScreen.kt
@@ -9,12 +9,25 @@ import com.example.welcome.presentation.RoutineState
 import com.example.welcome.presentation.RoutineEvents
 import com.example.welcome.presentation.RoutineViewModel
 
+//imports for the time picker
+import android.app.TimePickerDialog
+import android.widget.TimePicker
+import androidx.compose.ui.platform.LocalContext
+import java.util.*
+import java.util.Locale
+
+
 @Composable
 fun CreateRoutineScreen(
     state: RoutineState,
     onEvent: (RoutineEvents) -> Unit,
     onNavigateBack: () -> Unit
 ) {
+    //2 variables for the time picker
+    val context = LocalContext.current //enables Android to know exactly where to open up the TimePickerDialog.
+    val calendar = Calendar.getInstance() //creating a calender object. These objects can have the hour and minute which we need for the timepicker.
+
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -33,14 +46,40 @@ fun CreateRoutineScreen(
             modifier = Modifier.fillMaxWidth()
         )
 
-        OutlinedTextField(
-            value = state.time.value,
-            onValueChange = { onEvent(RoutineEvents.updateTime(it)) },
-            label = { Text("Time") },
+        //timepicker
+        Button(
+            onClick = {
+                val hour = calendar.get(Calendar.HOUR_OF_DAY)  //we can get the our with our calender object.
+                val minute = calendar.get(Calendar.MINUTE)  //we can get the minute too
+
+                //TimePickerDialog is basically a pop up clock that lets you choose time.
+                //recall context is where or how we want this clock to pop-up.
+                //line 59->is a callback(lambda function with 3 parameters) function that stores the user's time selections.
+                //locale allows for a more stable english version of time.
+                TimePickerDialog(
+                    context,
+                    { _: TimePicker, selectedHour: Int, selectedMinute: Int ->
+                        val amPm = if (selectedHour >= 12) "PM" else "AM"
+                        val hour12 = if (selectedHour % 12 == 0) 12 else selectedHour % 12
+                        val formattedTime = String.format(Locale.US,"%02d:%02d %s", hour12, selectedMinute, amPm)
+                        onEvent(RoutineEvents.updateTime(formattedTime))
+                    },
+                    hour,
+                    minute,
+                    false //therefore you expect 12 hour am/pm
+                ).show()
+            },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 16.dp)
-        )
+        ) {
+
+            //text that appears before you pick time and after it is set.
+            Text(
+                if (state.time.value.isEmpty()) "Pick Time" else "Time: ${state.time.value}"
+            )
+        }
+
 
         OutlinedTextField(
             value = state.recurrence.value,
